5eb964792f40f5f1dcf6f34597e072f10ba8efdb
daitao <dait@emspost.com.cn>
2020-12-25 18:30:04 +0800

    kafka配置对象创建1

--- src/main/java/com/opensource/message/config/KafkaProductConfig.java
+++ src/main/java/com/opensource/message/config/KafkaProductConfig.java
@@ -7,5 +7,19 @@ package com.opensource.message.config;
  */
 public class KafkaProductConfig {
     //kafka集群地址
-    private String services;
+    private String servers;
+    //Key反序列化类
+    private String keyDeserializer;
+    //value反序列化类
+    private String valueDeserializer;
+    //消费组ID
+    private String groupId;
+    //客户端ID
+    private String clientId;
+    //连接消费者心跳时间
+    private String heartbeatIntervalMs;
+    //检测消费者是否失效时间
+    private String sessionTimeOutMs;
+    //重新消费策略
+    private
 }
